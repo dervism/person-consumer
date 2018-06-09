a4ba36d0b37ae58d8ceb437186114c663ab83296
dervism <mail@dervis.no>
2018-06-10 00:53:07 +0200

    Initial commit

---
+++ src/main/java/no/dervis/pact/PersonService.java
@@ -0,0 +1,24 @@
+package no.dervis.pact;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
+public class PersonService {
+
+    private String host;
+    private String endpoint = "/person";
+
+    public PersonService(String host) {
+        this.host = host;
+    }
+
+    public Person getPerson(int id) throws IOException {
+        Person person = new ObjectMapper().readValue(Request.Get(host + endpoint + "/" + id).execute()
+                .returnContent().asString(), Person.class);
+
+        return person;
+    }
+
+}
---
+++ src/test/java/no/dervis/pact/PersonServiceTest.java
@@ -0,0 +1,58 @@
+package no.dervis.pact;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody;
+
+@ExtendWith(PactConsumerTestExt.class)
+class PersonServiceTest {
+
+    private static final String PERSON_API_URL = "/person";
+    private static final String CONSUMER = "person-consumer";
+    private static final String PROVIDER = "person-provider";
+
+    @Pact(consumer = CONSUMER, provider = PROVIDER)
+    public RequestResponsePact hasOnePerson(PactDslWithProvider builder) {
+        Map<String, String> headers = Collections.singletonMap("Content-Type", "application/json;charset=utf-8");
+
+        return builder
+                .given("a person exists")
+                .uponReceiving("a request for a person that exist")
+                .path(String.format("%s/%s", PERSON_API_URL, 0))
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .headers(headers)
+                .body(newJsonBody(body -> {
+                    body.numberType("id", 0);
+                    body.stringType("name", "Michael Johansen");
+                    body.numberType("age", 50);
+                }).build())
+                .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "hasOnePerson")
+    void verifyHasOnePerson(MockServer mockServer) throws IOException {
+        PersonService personService = new PersonService(mockServer.getUrl());
+        Person person = personService.getPerson(0);
+
+        Assertions.assertNotNull(person);
+        Assertions.assertEquals(0, person.getId());
+        Assertions.assertEquals("Michael Johansen", person.getName());
+        Assertions.assertEquals(50, person.getAge());
+    }
+
+}
